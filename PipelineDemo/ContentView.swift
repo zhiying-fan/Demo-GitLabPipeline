d2fd65cb22c175bb9a7b5be45758f8dd6a7f783d
Zhiying Fan <fanzhiying1992@gmail.com>
2022-05-26 19:50:46 +1000

    Initial Commit

---
+++ PipelineDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PipelineDemo
+//
+//  Created by Zhiying Fan on 26/5/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PipelineDemo/PipelineDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PipelineDemoApp.swift
+//  PipelineDemo
+//
+//  Created by Zhiying Fan on 26/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct PipelineDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
